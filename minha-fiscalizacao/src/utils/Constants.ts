493c6a2f8ef6e8ba585ee091c3df356de5bbda80
silvia_teixeira <quimicasilvia20@gmail.com>
2022-11-28 20:36:21 -0300

    change utils

--- minha-fiscalizacao/src/utils/Constants.ts
+++ minha-fiscalizacao/src/utils/Constants.ts
@@ -1,2 +1,5 @@
 export const URL_SERVER =
-  "http://localhost:3000"
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1:5173"
+    ? "http://localhost:3000"
+    : "https://fiscalizacao-orcamentaria-server.glitch.me";
